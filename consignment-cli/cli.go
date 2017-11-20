96cef859df40b5412d3aece692039492bbde5d5f
Ewan Valentine <ewan.valentine@bbc.co.uk>
2017-11-20 12:21:58 +0000

    ADDED cli

---
+++ consignment-cli/cli.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+
+	pb "github.com/ewanvalentine/shipper/consignment-service/proto/consignment"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+const (
+	address         = "localhost:50051"
+	defaultFilename = "consignment.json"
+)
+
+func parseFile(file string) (*pb.Consignment, error) {
+	var consignment *pb.Consignment
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	json.Unmarshal(data, &consignment)
+	return consignment, err
+}
+
+func main() {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Did not connect: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewShippingServiceClient(conn)
+
+	// Contact the server and print out its response.
+	file := defaultFilename
+	if len(os.Args) > 1 {
+		file = os.Args[1]
+	}
+
+	consignment, err := parseFile(file)
+
+	if err != nil {
+		log.Fatalf("Could not parse file: %v", err)
+	}
+
+	r, err := client.CreateConsignment(context.Background(), consignment)
+	if err != nil {
+		log.Fatalf("Could not greet: %v", err)
+	}
+	log.Printf("Created: %t", r.Created)
+
+	getAll, err := client.GetConsignments(context.Background(), &pb.GetRequest{})
+	if err != nil {
+		log.Fatalf("Could not list consignments: %v", err)
+	}
+	for _, v := range getAll.Consignments {
+		log.Println(v)
+	}
+}
