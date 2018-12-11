c672b24c5e57e1c6b61e09fd3d16e07326ddab1c
Andreas Amstutz <andreasamstutz@gmail.com>
2018-12-11 23:30:29 +0100

    Update to use go-micro service client

--- consignment-cli/cli.go
+++ consignment-cli/cli.go
@@ -31,7 +31,7 @@ func main() {
 	cmd.Init()
 
 	// Create new greeter client
-	client := pb.NewShippingServiceClient("go.micro.srv.consignment", microclient.DefaultClient)
+	client := pb.NewShippingService("go.micro.srv.consignment", microclient.DefaultClient)
 
 	// Contact the server and print out its response.
 	file := defaultFilename
